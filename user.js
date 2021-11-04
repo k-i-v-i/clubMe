21c8c42d3b97de98e165c1579714dc4ff6d03011
k-i-v-i <84327164+k-i-v-i@users.noreply.github.com>
2021-11-04 21:36:49 +0300

    Added the user schema

---
+++ user.js
@@ -0,0 +1,12 @@
+
+
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const UserSchema = new mongoose.Schema({
+    email:String,
+    name:String,
+    surname:String,
+    password:String,
+}) ;
+UserSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model("User",UserSchema);
